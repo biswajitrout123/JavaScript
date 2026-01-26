a1ce5b731e268426ea20cbfaa30cf376927e47d5
Biswajit Rout <routhappy9876@gmail.com>
2026-01-26 19:52:23 +0530

    Update in strings

--- CH_5_STRINGS/script.js
+++ CH_5_STRINGS/script.js
@@ -4,6 +4,11 @@ let newReplace = str.replace("hello" , "biswajit");
 console.log(newReplace);
 
 // Write a program to remove spaces from a string.
+let strr = "  hell o  ";
+let removeSpace = strr.replaceAll(" ", "");
+console.log(removeSpace);
+
+
 
 // Write a program to split a string into an array of words.
 
