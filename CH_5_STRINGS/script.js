cf90fef0dfde7ace3149a94467af717827da855a
Biswajit Rout <routhappy9876@gmail.com>
2026-01-27 18:34:30 +0530

    update in strings

--- CH_5_STRINGS/script.js
+++ CH_5_STRINGS/script.js
@@ -17,8 +17,24 @@ console.log(splitingWords);
 
 
 // Write a program to join an array of words into a string.
+let arr = ["hello", "rout"];
+let result = arr.join(" ");
+console.log(result);
+
+
 
 // Write a program to count occurrences of a character in a string.
+let str5 = "hello";
+let char = "l";
+let count = 0;
+for(let i = 0; i < str5.length; i++) {
+    if(str5[i] === char){
+        count++;
+    }
+}
+console.log(count);
 
 // Write a program using template literals to print a sentence. -->
+let name = `name: ${"Happy"}, Age: ${50}`;
+console.log(name);
 
