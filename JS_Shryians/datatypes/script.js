0d3c8b83f9545c2ee7451e7d3023889f606af8b1
Biswajit Rout <routhappy9876@gmail.com>
2026-03-03 19:37:27 +0530

    revising operator in js

--- JS_Shryians/datatypes/script.js
+++ JS_Shryians/datatypes/script.js
@@ -4,3 +4,15 @@ console.log(studentData);
 
 let a;
 console.log(a);
+
+let a2 = 9897n;
+console.log(typeof(a2));
+
+
+
+
+let c = typeof(null)
+console.log(c);
+
+let g = null;
+console.log(typeof(g));
---
+++ JS_Shryians/operators/script.js
@@ -0,0 +1,6 @@
+// ternary operator
+let score = 75;
+
+let grade = score >= 90 ? "A" : score >= 75 ? "B" : score >= 60 ? "C" : "fail";
+
+console.log(grade);
