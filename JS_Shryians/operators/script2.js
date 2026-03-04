276981db0818a6243f1f7efaad7edf7fdbaae159
Biswajit Rout <routhappy9876@gmail.com>
2026-03-04 20:11:24 +0530

    write stone-paper-scissors logic

---
+++ JS_Shryians/operators/script2.js
@@ -0,0 +1,31 @@
+// ROCK PAPER SCISSOR LOGIC
+function rps(user, computer) {
+    if(user === computer) {
+        console.log("Draw");
+    }
+    if(user === "stone" && computer === "scissors") {
+        console.log("user was Win");
+    }
+    else if (user === "scissors" && computer === "paper") {
+        console.log("User was Win");
+    }
+    else if (user === "paper" && computer === "stone"){
+        console.log("user was win");
+    }
+
+
+    else if (user === "scissors" && computer === "stone"){
+        console.log("user was win");
+    }
+    else if (user === "paper" && computer === "scissors"){
+        console.log("user was win");
+    }
+    else if (user === "stone" && computer === "paper"){
+        console.log("user was win");
+    }
+
+    
+}
+
+rps("stone", "scissors")
+console.log(rps("stone", "paper"));
