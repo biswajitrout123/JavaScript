373cd4cd7c097d97612241ec8d263b0d7737184c
Biswajit Rout <routhappy9876@gmail.com>
2026-03-07 18:56:19 +0530

    revise - IIFE functions

--- JS_Shryians/operators/script.js
+++ JS_Shryians/operators/script.js
@@ -6,27 +6,34 @@
 // console.log(grade);
 
 // Q-1
-function getGrade(score) {
-    if (score >= 0 && score <= 32) {
-        console.log("Fail");
-    }
-    else if (score >= 33 && score <= 59) {
-        console.log("D");
-    }
-    else if (score >= 60 && score <= 69) {
-        console.log("C");
-    }
-    else if (score >= 70 && score <= 79) {
-        console.log("B");
-    }
-    else if (score >= 80 && score <= 90) {
-        console.log("A");
-    }
-    else if (score >= 90 && score <= 100) {
-        return 'A+'
-    }
-    else {
-        console.log("NOT MATCH");
-    }
-}
-console.log((getGrade(99)));
+// function getGrade(score) {
+//     if (score >= 0 && score <= 32) {
+//         console.log("Fail");
+//     }
+//     else if (score >= 33 && score <= 59) {
+//         console.log("D");
+//     }
+//     else if (score >= 60 && score <= 69) {
+//         console.log("C");
+//     }
+//     else if (score >= 70 && score <= 79) {
+//         console.log("B");
+//     }
+//     else if (score >= 80 && score <= 90) {
+//         console.log("A");
+//     }
+//     else if (score >= 90 && score <= 100) {
+//         return 'A+'
+//     }
+//     else {
+//         console.log("NOT MATCH");
+//     }
+// }
+// console.log((getGrade(99)));
+
+
+
+
+(function() {
+    console.log("i am IIFE");
+})();
--- JS_Shryians/operators/script2.js
+++ JS_Shryians/operators/script2.js
@@ -1,31 +1,33 @@
 // ROCK PAPER SCISSOR LOGIC
-function rps(user, computer) {
-    if(user === computer) {
-        console.log("Draw");
-    }
-    if(user === "stone" && computer === "scissors") {
-        console.log("user was Win");
-    }
-    else if (user === "scissors" && computer === "paper") {
-        console.log("User was Win");
-    }
-    else if (user === "paper" && computer === "stone"){
-        console.log("user was win");
-    }
+// function rps(user, computer) {
+//     if(user === computer) {
+//         console.log("Draw");
+//     }
+//     if(user === "stone" && computer === "scissors") {
+//         console.log("user was Win");
+//     }
+//     else if (user === "scissors" && computer === "paper") {
+//         console.log("User was Win");
+//     }
+//     else if (user === "paper" && computer === "stone"){
+//         console.log("user was win");
+//     }
 
 
-    else if (user === "scissors" && computer === "stone"){
-        console.log("user was win");
-    }
-    else if (user === "paper" && computer === "scissors"){
-        console.log("user was win");
-    }
-    else if (user === "stone" && computer === "paper"){
-        console.log("user was win");
-    }
+//     else if (user === "scissors" && computer === "stone"){
+//         console.log("user was win");
+//     }
+//     else if (user === "paper" && computer === "scissors"){
+//         console.log("user was win");
+//     }
+//     else if (user === "stone" && computer === "paper"){
+//         console.log("user was win");
+//     }
 
     
-}
+// }
 
-rps("stone", "scissors")
-console.log(rps("stone", "paper"));
+// rps("stone", "scissors")
+// console.log(rps("stone", "paper"));
+
+
