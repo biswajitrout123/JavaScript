57e1567a0d5ff55f981afe2e627cede906b8540e
Biswajit Rout <routhappy9876@gmail.com>
2026-03-17 19:24:19 +0530

    Practicing Js arrays

--- JS_Shryians/Arrays/script.js
+++ JS_Shryians/Arrays/script.js
@@ -39,3 +39,49 @@
 
 
 // MAP - 
+// let arr = [11, 62, 3, 4, 25]
+// let newarr = arr.map(function (val) {
+//     return 12
+// })
+// console.log(newarr); // 12 12 12 12 12
+
+
+
+// let arr = [11, 62, 3, 4, 25];
+// let newarr = arr.map(function (val) {
+//     if(val > 10) return val;
+// })
+// console.log(newarr);
+
+// --------------------------------------------------------------
+
+// let arr = [1, 2, 3, 4, 5, 6];
+// let ans = arr.reduce(function(accumulatoor , val) {
+//     return accumulatoor + val;
+// }, 0);
+
+// console.log(ans);   //21
+
+
+// --------------------------------------------------------------------------
+// let arr = [1, 2, 3, 1, 4, 5, 6];
+
+// let va = arr.find(function (val) {
+//     return val === 1;
+// })
+
+// console.log(va);    //1
+
+//------
+
+let arr = [
+    { id: 1, key: 1 },
+    { id: 2, key: 2},
+    { id: 3, key: 1},
+];
+
+let va = arr.find(function (val) {
+    return val.key === 1;
+})
+
+console.log(va);
