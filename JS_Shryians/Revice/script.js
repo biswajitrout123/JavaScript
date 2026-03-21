91c7e11bc2c272ed50be29537921095a039dccb2
Biswajit Rout <routhappy9876@gmail.com>
2026-03-21 20:25:36 +0530

    Practicing Js From Starting

--- JS_Shryians/Revice/script.js
+++ JS_Shryians/Revice/script.js
@@ -1,7 +1,29 @@
-console.log(x === undefined); // true
-var x = 3;
+// console.log(x === undefined); // true
+// var x = 3;
 
-(function () {
-  console.log(x); // undefined
-  var x = "local value";
-})();
+// (function () {
+//   console.log(x); // undefined
+//   var x = "local value";
+// })();
+
+// -------------------------------------------------------------
+// console.log(x); // ReferenceError
+// const x = 3;
+
+// console.log(y); // ReferenceError
+// let y = 3;
+
+// -------------------------------------------------------------
+let answer = 42;
+answer = "Thanks for all the fish!";
+console.log(answer);
+
+
+// --------------------------------------------------------------
+let a = "37" - 7; // 30
+let b = "37" * 7; // 259
+
+// --------------------------------------------------------------
+
+let c = parseInt("101", 2); // 5
+console.log(c);
