e51611beea8fd5f9259af6299981c46ab05d0650
Biswajit Rout <routhappy9876@gmail.com>
2026-04-03 14:04:34 +0530

    Dom Revising And toogle

---
+++ JS_Shryians/Dom/Dom_Manipulationn/script.js
@@ -0,0 +1,13 @@
+//  TO ADD CLASS IN HTML TAG THROUGH 
+let h1 = document.querySelector('h1');
+h1.classList.add("hulu"); // IT WRITES THE CLASS NAME IS HULU;
+
+
+// IT BYDEFAULT CLASS WAS THERE IN HTML TAG THEN YOU WANT TO REMOVE THEN 
+h1.classList.remove('hulu');
+
+// TOGGLE DOING THAT IF CLASS WAS THERE THEN IT REMOVE AND IF NOT THERE ITS ADD:
+h1.classList.toggle('hulu');
+
+
+
