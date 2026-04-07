a41e3e5e8c815b40b3e5efc986a83ff434f53cf6
Biswajit Rout <routhappy9876@gmail.com>
2026-04-07 16:52:03 +0530

    Revising JS

--- JS_Shryians/Dom/DOM_EVENT_LISTNER/script.js
+++ JS_Shryians/Dom/DOM_EVENT_LISTNER/script.js
@@ -1,7 +1,17 @@
-let input = document.querySelector('input');
-input.addEventListener('input', function(evt){
-    if(evt.data !== null) {
-        console.log(evt.data);
-    }
+// let input = document.querySelector('input');
+// input.addEventListener('input', function(evt){
+//     if(evt.data !== null) {
+//         console.log(evt.data);
+//     }
 
+// })
+
+
+let sel = document.querySelector('select');
+let device = document.querySelector('#device');
+
+sel.addEventListener('change', function(evt){  
+    device.textContent = "Device Selected"  
+    device.textContent = `${evt.target.value} is Selected`;
+    
 })
