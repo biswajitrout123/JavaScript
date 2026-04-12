fb824fa555323c3e044d6c39db55503ce2f3b426
Biswajit Rout <routhappy9876@gmail.com>
2026-04-12 22:29:31 +0530

    Practice JS

---
+++ JS_Shryians/Dom/DOM_EVENT_LISTNER/QuestonsSolvng/script.js
@@ -0,0 +1,3 @@
+// ADD A TTLE ATTRBUTE TO A DV DiRCTLE
+let div = document.querySelector('div');
+div.setAttribute('title', 'Some info');
--- JS_Shryians/Dom/DOM_EVENT_LISTNER/practce/script.js
+++ JS_Shryians/Dom/DOM_EVENT_LISTNER/practce/script.js
@@ -1,4 +1,7 @@
 const img = document.querySelector('img');
 // console.log(img.src);
 // console.log(img.getAttribute('src'));
-img.setAttribute('src', 'https://images.unsplash.com/photo-1774579892278-e305439f05dd?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
+// img.setAttribute('src', 'https://images.unsplash.com/photo-1774579892278-e305439f05dd?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
+
+let a = document.querySelector('a');
+a.href = 'https://www.google.com/?zx=1776012208908';
